29b244cfb0e8396b0040172c634b7d07f7aba751
Sunil Yadav <sunilyadavwork2003@gmail.com>
2024-02-18 04:27:57 +0530

    Created Models: Platform, Social Profiles, Referral.

--- reachify/reachapp/admin.py
+++ reachify/reachapp/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Platform, SocialProfile, Referral
 
-# Register your models here.
+
+@admin.register(Platform)
+class PlatformAdmin(admin.ModelAdmin):
+    list_display = ['name', 'link', 'is_active', 'created']
+    readonly_fields = ['created']
+
+
+@admin.register(SocialProfile)
+class SocialProfileAdmin(admin.ModelAdmin):
+    list_display = ['member', 'platform', 'username', 'old_username', 'is_active', 'created']
+    readonly_fields = ['created']
+
+
+@admin.register(Referral)
+class ReferralAdmin(admin.ModelAdmin):
+    list_display = ['member', 'code', 'is_completed', 'created']
+    readonly_fields = ['created']
--- reachify/reachapp/models.py
+++ reachify/reachapp/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django_extensions.db.models import TimeStampedModel
 
-# Create your models here.
+from reachify.users.models import Member
+
+
+class Platform(TimeStampedModel):
+    name = models.CharField(max_length=100, unique=True)
+    link = models.URLField()
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+
+
+class SocialProfile(TimeStampedModel):
+    member = models.ForeignKey(Member, on_delete=models.CASCADE)
+    platform = models.ForeignKey(Platform, on_delete=models.CASCADE)
+    username = models.CharField(max_length=150, unique=True)
+    old_username = models.CharField(max_length=150, null=True, blank=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.member.username} - {self.platform.name}"
+
+
+class Referral(TimeStampedModel):
+    member = models.ForeignKey(Member, on_delete=models.CASCADE)
+    code = models.CharField(max_length=80, unique=True)
+    is_completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.member.username} - {self.code}"
